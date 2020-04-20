53817f4c91086e5ddc2b604e3ebeb00c13485d68
PrabhavKarve <karve.prabhav@gmail.com>
2020-04-20 15:11:13 +0530

    1 file modified, 1 added

--- Hello.js
+++ Hello.js
@@ -3,12 +3,13 @@ import './style.css';
 import Greet from './components/Greet';
 import Greetclass from './components/Greetclass';
 import Message from './components/message';
+import Counter from './components/counter';
 class App extends Component, Greetclass, Message{
 
   render() {
     return (
       <div>
-        <Message/>
+        <Counter/>
       </div>
     );
   }
